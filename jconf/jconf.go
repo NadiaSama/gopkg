07018fd9a0b02ad38399b4393bb5290c869fc3bd
emanon <notexist@not.exist>
2020-07-04 14:01:15 +0800

    1.add update and test 2.make confStruct conf field settable

--- jconf/jconf.go
+++ jconf/jconf.go
@@ -14,8 +14,8 @@ type (
 	}
 
 	confStruct struct {
-		meta map[string]structMeta
-		conf atomic.Value
+		meta    map[string]structMeta
+		confPtr atomic.Value //conf hold pointer value of struct. to make value settable
 	}
 )
 
@@ -25,18 +25,17 @@ var (
 
 //buildConfStruct build confStruct
 func buildConfStruct(s interface{}) (*confStruct, error) {
-	value := reflect.Indirect(reflect.ValueOf(s))
-	kind := value.Kind()
-	if kind != reflect.Struct {
+	pv := reflect.ValueOf(s)
+	kind := pv.Kind()
+	if kind != reflect.Ptr {
 		return nil, ErrInvalidConf
 	}
-
-	if !value.CanSet() {
-		return nil, errors.WithMessage(ErrInvalidConf, "conf can not set")
+	orig := pv.Elem()
+	if kind := orig.Kind(); kind != reflect.Struct {
+		return nil, ErrInvalidConf
 	}
-
-	t := value.Type()
-	amount := value.NumField()
+	amount := orig.NumField()
+	t := orig.Type()
 	ret := &confStruct{
 		meta: make(map[string]structMeta, amount),
 	}
@@ -53,19 +52,51 @@ func buildConfStruct(s interface{}) (*confStruct, error) {
 		}
 	}
 
-	ret.conf.Store(value.Interface())
+	copy := copyStructToPtr(orig)
+	ret.confPtr.Store(copy)
 	return ret, nil
 }
 
-//load conf and store into dst. dst should be pointer type of conf
+//load conf and store into dst. dst should be pointer type of struct
 func (cf *confStruct) load(dst interface{}) error {
-	val := cf.conf.Load()
-	st := reflect.PtrTo(reflect.TypeOf(val))
+	val := cf.confPtr.Load().(reflect.Value)
+	st := val.Type()
 	dt := reflect.TypeOf(dst)
 	if st != dt {
 		return errors.Errorf("unmatch conf type want '%v' got '%v'", st, dt)
 	}
-	orig := reflect.Indirect(reflect.ValueOf(dst))
-	orig.Set(reflect.ValueOf(val))
+	target := reflect.Indirect(reflect.ValueOf(dst))
+	target.Set(val.Elem())
 	return nil
+}
+
+func (cf *confStruct) update(update map[string]interface{}) error {
+	conf := copyStructToPtr(cf.confPtr.Load().(reflect.Value).Elem())
+	elem := conf.Elem()
+	for key, val := range update {
+		meta, ok := cf.meta[key]
+		if !ok {
+			return errors.Errorf("unkown update filed '%s'", key)
+		}
+		if t := reflect.TypeOf(val); !t.AssignableTo(meta.Type) {
+			return errors.Errorf("unassignble update field '%s' want '%v' got '%v'",
+				key, meta.Type, t)
+		}
+		field := elem.Field(meta.Idx)
+		field.Set(reflect.ValueOf(val))
+	}
+	cf.confPtr.Store(conf)
+	return nil
+}
+
+func copyStructToPtr(src reflect.Value) reflect.Value {
+	ret := reflect.New(src.Type())
+	t := ret.Elem()
+	amount := src.NumField()
+	for i := 0; i < amount; i++ {
+		sf := src.Field(i)
+		df := t.Field(i)
+		df.Set(sf)
+	}
+	return ret
 }
--- jconf/jconf_test.go
+++ jconf/jconf_test.go
@@ -13,8 +13,9 @@ type (
 	}
 
 	s2 struct {
-		Name string `json:"name"`
-		Age  int    `json:"age"`
+		Name string  `json:"name"`
+		Age  int     `json:"age"`
+		Val  float64 `json:"val"`
 	}
 )
 
@@ -33,11 +34,6 @@ func TestParse(t *testing.T) {
 		parsed.meta["age"].Idx != 1 || parsed.meta["age"].Type.String() != "int" {
 		t.Errorf("parse fail %v", parsed)
 	}
-	ps2.Age = 24
-	val := parsed.conf.Load().(s2)
-	if val.Age != 23 || val.Name != "hehe" {
-		t.Errorf("invalid conf %v", val)
-	}
 }
 
 func TestLoad(t *testing.T) {
@@ -53,7 +49,35 @@ func TestLoad(t *testing.T) {
 	if err := parsed.load(d2); err == nil {
 		t.Errorf("test d2 fail %v", err)
 	}
+
 	if err := parsed.load(&d2); err != nil || d2.Age != 23 || d2.Name != "hehe" {
 		t.Errorf("test &d2 fail %v", err)
 	}
+}
+
+func TestUpdate(t *testing.T) {
+	ps2 := &s2{Age: 23, Name: "hehe", Val: 23.3}
+	parsed, _ := buildConfStruct(ps2)
+
+	if err := parsed.update(map[string]interface{}{"val": 11.0, "name": "haha", "key": 1}); err == nil {
+		t.Errorf("update bad data failed")
+	}
+	if err := parsed.update(map[string]interface{}{"val": 23, "name": 1.0}); err == nil {
+		t.Errorf("update bad fmt failed")
+	}
+	if err := parsed.update(map[string]interface{}{"val": 11.0, "name": "new", "age": 12}); err != nil {
+		t.Errorf("update failed %v", err.Error())
+	}
+
+	var d2 s2
+	parsed.load(&d2)
+	if d2.Name != "new" || d2.Age != 12 || d2.Val != 11.0 {
+		t.Errorf("update failed %v", d2)
+	}
+	parsed.update(map[string]interface{}{"val": 23.0, "name": "u2"})
+	parsed.load(&d2)
+	if d2.Name != "u2" || d2.Age != 12 || d2.Val != 23.0 {
+		t.Errorf("update failed %v", d2)
+	}
+
 }
